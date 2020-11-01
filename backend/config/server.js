d324daeed08551441e3b081d80c9c7cd0a674e14
jp <jptick@gmail.com>
2020-11-01 17:53:37 -0300

    First commit

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f5242fce2267d87245f88dd8ac71dc3a'),
+    },
+  },
+});
