cd192e26f718bbba2f1e87ae650517dae05ceca8
jp <jptick@gmail.com>
2020-11-09 19:47:07 -0300

    Create dinamic header

--- landing-page/src/pages/index.tsx
+++ landing-page/src/pages/index.tsx
@@ -15,10 +15,11 @@ import Footer from 'components/Footer'
 import JsonSchema from 'components/JsonSchema'
 import client from 'graphql/client'
 import GET_LANDING_PAGE from 'graphql/queries/getLandingPage'
+import { LandingPageProps } from 'types/api'
 
-const Index = () => (
+const Index = ({ logo, header }: LandingPageProps) => (
   <>
-    <SectionHero />
+    <SectionHero logo={logo} header={header} />
     <SectionAboutProject />
     <SectionTech />
     <SectionConcepts />
@@ -35,8 +36,6 @@ const Index = () => (
 
 export const getStaticProps: GetStaticProps = async () => {
   const { landingPage } = await client.request(GET_LANDING_PAGE)
-
-  console.log(landingPage)
 
   return {
     props: {
