da574bb47bfe1caebc5c5e9553beefe73fe97ee6
jp <jptick@gmail.com>
2020-11-10 17:48:47 -0300

    Create dinamic sectionFaq

--- landing-page/src/types/api.ts
+++ landing-page/src/types/api.ts
@@ -92,6 +92,16 @@ export type SectionReviewsProps = {
   reviews: Review[]
 }
 
+export type Question = {
+  question: string
+  answer: string
+}
+
+export type SectionFaqProps = {
+  title: string
+  questions: Question[]
+}
+
 export type LandingPageProps = {
   logo: LogoProps
   header: HeaderProps
@@ -103,4 +113,5 @@ export type LandingPageProps = {
   pricingBox: PricingBoxProps
   sectionAboutUs: SectionAboutUsProps
   sectionReviews: SectionReviewsProps
+  sectionFaq: SectionFaqProps
 }
